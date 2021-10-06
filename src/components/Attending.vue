89aa0ee01aa3482ae27a2bc6cc52ad962fee152d
BacHaddad <br.yhaddad@gmail.com>
2021-10-06 16:55:32 +0200

    fixed attending component

--- src/components/Attending.vue
+++ src/components/Attending.vue
@@ -1,42 +1,55 @@
 <template>
   <section class="main-calender">
-    
+    <article v-for="(event, index) in attending" :key="index" class="info">
+        <h2>{{ event.name }}</h2>
+        <small> {{event.date}} </small>
+        <small> {{event.time}} </small>
+    </article>
    
-      <Event v-on:addToCalender="sendToCalender" v-on:removeFromCalender="deleteFromCalender" v-for="(event, index) in attending" :key="index" :event="event" />
+      <!-- <Event v-on:addToCalender="sendToCalender" v-on:removeFromCalender="deleteFromCalender" v-for="(event, index) in attending" :key="index" :event="event" /> -->
 
   </section>
 </template>
 
 <script>
-import Event from '@/components/Event';
+// import Event from '@/components/Event';
 
 export default {
   name: 'Attending',
-  components: { Event },
+  // components: { Event },
   props: {
     attending: Array,
   },
   methods: {
-    sendToCalender(event) {
-      console.log("log event from  eventsList", event);
-      this.$emit('addToCalender', event);
-    },
-    deleteFromCalender(event) {
-      console.log("log event from  eventsList", event);
-      this.$emit('removeFromCalender', event);
-    }
+    // sendToCalender(event) {
+    //   console.log("log event from  eventsList", event);
+    //   this.$emit('addToCalender', event);
+    // },
+    // deleteFromCalender(event) {
+    //   console.log("log event from  eventsList", event);
+    //   this.$emit('removeFromCalender', event);
+    // }
   },
 };
 </script>
 
 <style scoped>
 .main-calender {
-    max-width: 15rem;
+  background-color: #fff;
+    min-width: 15rem;
     position: absolute;
   right: 5%;
   top: 10%;
+  display: flex;
+  flex-wrap: wrap;
+  
 }
 
-
+.info {
+  border: solid thin #000;
+  border-radius: 3rem;
+  padding: 2rem;
+  margin: 1.5rem;
+}
 
 </style>
