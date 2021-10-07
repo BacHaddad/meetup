e62a634208617b43aa77a149c2764254033173cf
BacHaddad <br.yhaddad@gmail.com>
2021-10-07 11:51:06 +0200

    finished testing localStorage

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -3,17 +3,29 @@ import App from '@/App.vue';
 import 'jest-localstorage-mock';
 
 describe('App', () => {
-  it('should test if an event is pushed to localStorage', async () => {
-    const wrapper = mount(App);
-    const KEY = 'events-attending',
-    VALUE = [{ id: 1, name: 'Not a Sunrise', date: '2021/11/15', time: '14:00', going: false, review: '' }];
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(App);
+  });
+
+  it('should test if an event is pushed into attending array i data', async () => {
     const data = { id: 1, name: 'Not a Sunrise', date: '2021/11/15', time: '14:00', going: false, review: '' };
-    const StriData = JSON.stringify(VALUE)
+
     await wrapper.vm.addToAttending(data);
     const eventsAttending = wrapper.vm.attending.length;
+
     expect(eventsAttending).toBe(1);
+  });
+
+  it('should test if data is stored to localStorage', () => {
+    const KEY = 'events-attending',
+      VALUE = [{ id: 1, name: 'Not a Sunrise', date: '2021/11/15', time: '14:00', going: false, review: '' }];
+
+    const StriData = JSON.stringify(VALUE);
+
     expect(localStorage.setItem).toHaveBeenLastCalledWith(KEY, StriData);
     expect(localStorage.__STORE__[KEY]).toBe(StriData);
     expect(Object.keys(localStorage.__STORE__).length).toBe(1);
+    console.log(' local storage data :', Object.keys(localStorage.__STORE__));
   });
 });
